74d863bffcd6c7bde8473542dae1a98455be40af
Nathan Kanigsberg <nathan@kanigsberg.com>
2021-01-10 15:22:23 -0500

    rename files

--- obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,70 +13,70 @@ namespace blazorProject.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "c:\Users\Nathan\Desktop\Blazor App\blazorProject\_Imports.razor"
+#line 1 "c:\Users\Nathan\Desktop\BlazorApps\blazor-project\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\Nathan\Desktop\Blazor App\blazorProject\_Imports.razor"
+#line 2 "c:\Users\Nathan\Desktop\BlazorApps\blazor-project\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "c:\Users\Nathan\Desktop\Blazor App\blazorProject\_Imports.razor"
+#line 3 "c:\Users\Nathan\Desktop\BlazorApps\blazor-project\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "c:\Users\Nathan\Desktop\Blazor App\blazorProject\_Imports.razor"
+#line 4 "c:\Users\Nathan\Desktop\BlazorApps\blazor-project\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "c:\Users\Nathan\Desktop\Blazor App\blazorProject\_Imports.razor"
+#line 5 "c:\Users\Nathan\Desktop\BlazorApps\blazor-project\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "c:\Users\Nathan\Desktop\Blazor App\blazorProject\_Imports.razor"
+#line 6 "c:\Users\Nathan\Desktop\BlazorApps\blazor-project\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "c:\Users\Nathan\Desktop\Blazor App\blazorProject\_Imports.razor"
+#line 7 "c:\Users\Nathan\Desktop\BlazorApps\blazor-project\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "c:\Users\Nathan\Desktop\Blazor App\blazorProject\_Imports.razor"
+#line 8 "c:\Users\Nathan\Desktop\BlazorApps\blazor-project\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "c:\Users\Nathan\Desktop\Blazor App\blazorProject\_Imports.razor"
+#line 9 "c:\Users\Nathan\Desktop\BlazorApps\blazor-project\_Imports.razor"
 using blazorProject;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "c:\Users\Nathan\Desktop\Blazor App\blazorProject\_Imports.razor"
+#line 10 "c:\Users\Nathan\Desktop\BlazorApps\blazor-project\_Imports.razor"
 using blazorProject.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using blazorProject.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "c:\Users\Nathan\Desktop\Blazor App\blazorProject\Shared\SurveyPrompt.razor"
+#line 12 "c:\Users\Nathan\Desktop\BlazorApps\blazor-project\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
